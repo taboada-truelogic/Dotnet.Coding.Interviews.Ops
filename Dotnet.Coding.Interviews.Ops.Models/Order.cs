06fbbf61da492dde562ea0a41d7ee3bce585ffc7
Hernan Taboada <htaboada@truelogic.io>
2024-10-22 15:16:38 -0300

    refactor: rename ProductId to InventoryId

--- Dotnet.Coding.Interviews.Ops.Models/Order.cs
+++ Dotnet.Coding.Interviews.Ops.Models/Order.cs
@@ -3,7 +3,7 @@
 public class Order
 {
     public int Id { get; set; }
-    public int ProductId { get; set; }
+    public int InventoryId { get; set; }
     public int Quantity { get; set; }
     public bool IsPaid { get; set; }
 }
